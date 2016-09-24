99774ce3183f0ff0f00c4c809536f3cd726ce404
kimsehwa <negabaro@gmail.com>
2016-09-25 05:12:43 +0900

    bucket_list test 実装

--- app/helpers/bucket_list_helper.rb
+++ app/helpers/bucket_list_helper.rb
@@ -2,11 +2,43 @@ module BucketListHelper
   
  require 'aws-sdk' 
   @@client =""
-  BUCKET_NAME = 'your bucket name'
+  BUCKET_NAME = 'filmarks-env-list'
+
   def bucket_list
 
      connect
      @@client.list_buckets.buckets.map(&:name)
+  end
+
+  def file_list
+    #@@client.list_objects(:bucket => 'filmarks-env-list').contents.each do |object|
+    #    puts object.key
+    #end
+    
+    resp = @@client.list_objects(bucket: BUCKET_NAME)
+    resp.contents.map(&:key)
+    
+  end
+
+  def file_download 
+
+    File.open("/home/kaka/docker/mang_env_val_with_s3/tmp/kaka22", "w") do |file|   @@client.get_object(bucket: BUCKET_NAME, key: "envrc_unicorn_staging") do |chunk|     file.write chunk   end end
+
+
+  end
+
+  def file_upload
+
+    file = File.open('/home/kaka/docker/mang_env_val_with_s3/tmp/kaka')
+    #file = File.open('/tmp/keke/haha')
+    #file_name = File.basename('/tmp/keke2')
+    file_name = File.basename('koko')
+
+    @@client.put_object(
+        bucket: BUCKET_NAME,
+          body: file,
+            key: file_name
+    )
   end
 
   # def self.get_files_list
